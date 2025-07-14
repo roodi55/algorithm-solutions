9c37f34129bfa78476e8bc8b5f6533f148264885
sjlee22 <zhu__ee23@naver.com>
2025-07-14 18:29:40 +0900

    test

--- src/com/baekjoon/step01/Baekjoon_1000.java
+++ src/com/baekjoon/step01/Baekjoon_1000.java
@@ -8,7 +8,6 @@ public class Baekjoon_1000 {
 	 * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 	 */
 	public static void main(String[] args) {
-		long startTime = System.currentTimeMillis(); // 코드 시작 시간
 		
 		Scanner in = new Scanner(System.in);
 		int a = in.nextInt();
@@ -17,13 +16,7 @@ public class Baekjoon_1000 {
 		
 		System.out.println(a+b);	
 		
-		long endTime = System.currentTimeMillis(); // 코드 끝난 시간
 
-		long durationTimeSec = endTime - startTime;
-	    
-		System.out.println(durationTimeSec + "m/s");
-		System.out.println((durationTimeSec / 1000) + "sec");
-		
 	}
 	
 }
